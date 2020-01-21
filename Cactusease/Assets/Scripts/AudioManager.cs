9205ad095460829457c92c3315197ab53d29c7aa
Farad2020 <fnirc@mail.ru>
2020-01-21 22:21:57 +0600

    menu scene added

---
+++ Cactusease/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=6OT43pvUyfY&t=285s
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
